4c89d92fc61c6d7dfd3052a93f232dcf547f752c
Tane10 <tane2@hotmail.co.uk>
2019-09-28 09:06:26 +0100

    welcome screen with images

--- OCA_Kiwi/App.js
+++ OCA_Kiwi/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Homescreen from "./welcomes_screen"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Homescreen/>
     </View>
   );
 }
